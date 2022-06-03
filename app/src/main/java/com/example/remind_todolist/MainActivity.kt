cc3a416d6f3e994402b9b51baba3357b8acff548
YHKwon <ssugy2@gamil.com>
2022-06-03 17:06:05 +0900

    프래그먼트 추가 1. goal 2. todo 3. setting

--- app/src/main/java/com/example/remind_todolist/MainActivity.kt
+++ app/src/main/java/com/example/remind_todolist/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.remind_todolist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.remind_todolist.bases.BaseActivity
 import com.example.remind_todolist.databinding.ActivityMainBinding
 
+/**
+ * 1. 탭레이아웃 작성
+ * 2. 바텀네비게이션
+ * 3. 뷰페이저 연결
+ *
+ */
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
